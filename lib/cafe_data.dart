7ce69517ec57c992982bb8b0ef62188a76f07da8
Vanya <ivanpes530@gmail.com>
2026-03-30 11:09:05 +0300

    add cafe data and file for clustering

---
+++ lib/cafe_data.dart
@@ -0,0 +1,17 @@
+class Cafe {
+  final String name;
+  final int gridX;
+  final int gridY;
+  int? clusterId;
+  Cafe(this.name, this.gridX, this.gridY);
+}
+
+final List<Cafe> allCafes = [
+  Cafe("Старбукс", 185, 129),
+  Cafe("Сибирские блины", 185, 119),
+  Cafe("Главная столовая", 185, 119),
+  Cafe("Кафе во 2 корпусе", 152, 164),
+  Cafe("Столовая 2 корпус", 152, 164),
+  Cafe("Сырбор", 183, 45),
+  Cafe("Абрикос", 16, 34)
+];
